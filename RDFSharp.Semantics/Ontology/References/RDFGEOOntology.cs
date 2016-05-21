6302de06d394733aa540a697bb72cc8fb5556d83
Marco De Salvo <mdesalvo@users.noreply.github.com>
2016-05-22 01:11:22 +0200

    (FIX) Typo in comment;

--- RDFSharp.Semantics/Ontology/References/RDFGEOOntology.cs
+++ RDFSharp.Semantics/Ontology/References/RDFGEOOntology.cs
@@ -20,7 +20,7 @@ using RDFSharp.Model;
 namespace RDFSharp.Semantics {
 
     /// <summary>
-    /// RDFFOAFOntology represents an OWL-DL ontology implementation of GEO vocabulary
+    /// RDFGEOOntology represents an OWL-DL ontology implementation of W3C GEO vocabulary
     /// </summary>
     public static class RDFGEOOntology {
 
