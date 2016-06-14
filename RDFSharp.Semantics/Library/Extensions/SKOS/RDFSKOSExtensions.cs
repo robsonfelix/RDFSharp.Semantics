a06ccc7c756c772cd097d88d512ddd8782afdd50
Marco De Salvo <mdesalvo@users.noreply.github.com>
2016-06-15 00:15:31 +0200

    (NEW) More work on SKOS extensions;

--- RDFSharp.Semantics/Library/Extensions/SKOS/RDFSKOSExtensions.cs
+++ RDFSharp.Semantics/Library/Extensions/SKOS/RDFSKOSExtensions.cs
@@ -26,14 +26,27 @@ namespace RDFSharp.Semantics.SKOS {
 
         #region Methods
 
-        #region Add
+        #region AddFact
         /// <summary>
         /// Adds the given fact and its "skos:Concept" classtype relation to the ontology data
         /// </summary>
-        public static RDFOntologyData AddSKOSConcept(this RDFOntologyData ontologyData, RDFOntologyFact skosConcept) {
+        public static RDFOntologyData AddSKOSConcept(this RDFOntologyData ontologyData, 
+                                                     RDFOntologyFact skosConcept) {
             if (ontologyData != null && skosConcept != null) {
-                ontologyData.AddFact(skosConcept);
-                ontologyData.AddClassTypeRelation(skosConcept, RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.CONCEPT.ToString()));
+
+                //Enforce taxonomy checks
+                if (!ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConcept, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT_SCHEME.ToRDFOntologyClass()))     && 
+                    !ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConcept, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.COLLECTION.ToRDFOntologyClass()))         && 
+                    !ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConcept, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.ORDERED_COLLECTION.ToRDFOntologyClass()))) {
+
+                     //Declare the fact
+                     ontologyData.AddFact(skosConcept);
+
+                     //Add its classtype
+                     ontologyData.AddClassTypeRelation(skosConcept, RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.CONCEPT.ToString()));
+
+                }
+
             }
             return ontologyData;
         }
@@ -41,10 +54,23 @@ namespace RDFSharp.Semantics.SKOS {
         /// <summary>
         /// Adds the given fact and its "skos:ConceptScheme" classtype relation to the ontology data
         /// </summary>
-        public static RDFOntologyData AddSKOSConceptScheme(this RDFOntologyData ontologyData, RDFOntologyFact skosConceptScheme) {
+        public static RDFOntologyData AddSKOSConceptScheme(this RDFOntologyData ontologyData, 
+                                                           RDFOntologyFact skosConceptScheme) {
             if (ontologyData != null && skosConceptScheme != null) {
-                ontologyData.AddFact(skosConceptScheme);
-                ontologyData.AddClassTypeRelation(skosConceptScheme, RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.CONCEPT_SCHEME.ToString()));
+
+                //Enforce taxonomy checks
+                if (!ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptScheme, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass()))            &&
+                    !ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptScheme, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.COLLECTION.ToRDFOntologyClass()))         &&
+                    !ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptScheme, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.ORDERED_COLLECTION.ToRDFOntologyClass()))) {
+
+                     //Declare the fact
+                     ontologyData.AddFact(skosConceptScheme);
+
+                     //Add its classtype
+                     ontologyData.AddClassTypeRelation(skosConceptScheme, RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.CONCEPT_SCHEME.ToString()));
+
+                }
+
             }
             return ontologyData;
         }
@@ -52,10 +78,23 @@ namespace RDFSharp.Semantics.SKOS {
         /// <summary>
         /// Adds the given fact and its "skos:Collection" classtype relation to the ontology data
         /// </summary>
-        public static RDFOntologyData AddSKOSCollection(this RDFOntologyData ontologyData, RDFOntologyFact skosCollection) {
+        public static RDFOntologyData AddSKOSCollection(this RDFOntologyData ontologyData, 
+                                                        RDFOntologyFact skosCollection) {
             if (ontologyData != null && skosCollection != null) {
-                ontologyData.AddFact(skosCollection);
-                ontologyData.AddClassTypeRelation(skosCollection, RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.COLLECTION.ToString()));
+
+                //Enforce taxonomy checks
+                if (!ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosCollection, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass()))            &&
+                    !ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosCollection, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT_SCHEME.ToRDFOntologyClass()))     &&
+                    !ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosCollection, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.ORDERED_COLLECTION.ToRDFOntologyClass()))) {
+
+                     //Declare the fact
+                     ontologyData.AddFact(skosCollection);
+
+                     //Add its classtype
+                     ontologyData.AddClassTypeRelation(skosCollection, RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.COLLECTION.ToString()));
+
+                }
+
             }
             return ontologyData;
         }
@@ -63,10 +102,126 @@ namespace RDFSharp.Semantics.SKOS {
         /// <summary>
         /// Adds the given fact and its "skos:OrderedCollection" classtype relation to the ontology data
         /// </summary>
-        public static RDFOntologyData AddSKOSOrderedCollection(this RDFOntologyData ontologyData, RDFOntologyFact skosOrderedCollection) {
+        public static RDFOntologyData AddSKOSOrderedCollection(this RDFOntologyData ontologyData, 
+                                                               RDFOntologyFact skosOrderedCollection) {
             if (ontologyData != null && skosOrderedCollection != null) {
-                ontologyData.AddFact(skosOrderedCollection);
-                ontologyData.AddClassTypeRelation(skosOrderedCollection, RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.ORDERED_COLLECTION.ToString()));
+
+                //Enforce taxonomy checks
+                if (!ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosOrderedCollection, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass()))        &&
+                    !ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosOrderedCollection, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT_SCHEME.ToRDFOntologyClass())) &&
+                    !ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosOrderedCollection, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.COLLECTION.ToRDFOntologyClass())))     {
+
+                    //Declare the fact
+                    ontologyData.AddFact(skosOrderedCollection);
+
+                    //Add its classtype
+                    ontologyData.AddClassTypeRelation(skosOrderedCollection, RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.ORDERED_COLLECTION.ToString()));
+
+                }
+
+            }
+            return ontologyData;
+        }
+        #endregion
+
+        #region AddRelation
+        /// <summary>
+        /// Adds the given "skosConcept skos:inScheme skosConceptScheme" assertion to the ontology data
+        /// </summary>
+        public static RDFOntologyData AddSKOSInScheme(this RDFOntologyData ontologyData,
+                                                      RDFOntologyFact skosConcept,
+                                                      RDFOntologyFact skosConceptScheme) {
+            if (ontologyData != null && skosConcept != null && skosConceptScheme != null && !skosConcept.Equals(skosConceptScheme)) {
+
+                //Enforce taxonomy checks
+                if (ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConcept,       RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass()))        &&
+                    ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptScheme, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT_SCHEME.ToRDFOntologyClass()))) {
+
+                    // skosConcept -> skos:inScheme -> skosConceptScheme
+                    ontologyData.AddAssertionRelation(skosConcept, (RDFOntologyObjectProperty)RDFSKOSOntology.Instance.Model.PropertyModel.SelectProperty(RDFVocabulary.SKOS.IN_SCHEME.ToString()), skosConceptScheme);
+                }
+
+            }
+            return ontologyData;
+        }
+
+        /// <summary>
+        /// Adds the given "skosCollection skos:member [skosConcept | skosConceptScheme]" assertion to the ontology data
+        /// </summary>
+        public static RDFOntologyData AddSKOSMember(this RDFOntologyData ontologyData,
+                                                    RDFOntologyFact skosCollection,
+                                                    RDFOntologyFact skosConceptOrScheme) {
+            if (ontologyData != null && skosConceptOrScheme != null && !skosCollection.Equals(skosConceptOrScheme)) {
+
+                //Enforce taxonomy checks
+                if (ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosCollection,       RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.COLLECTION.ToRDFOntologyClass()))      &&
+                    (ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptOrScheme, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass()))         ||
+                     ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptOrScheme, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT_SCHEME.ToRDFOntologyClass())))) {
+
+                    // skosCollection -> skos:member -> skosConceptOrScheme
+                    ontologyData.AddAssertionRelation(skosCollection, (RDFOntologyObjectProperty)RDFSKOSOntology.Instance.Model.PropertyModel.SelectProperty(RDFVocabulary.SKOS.MEMBER.ToString()), skosConceptOrScheme);
+                }
+
+            }
+            return ontologyData;
+        }
+
+        /// <summary>
+        /// Adds the given "skosConcept skos:TopConceptOf skosConceptScheme" assertion to the ontology data
+        /// </summary>
+        public static RDFOntologyData AddSKOSTopConceptOf(this RDFOntologyData ontologyData, 
+                                                          RDFOntologyFact skosConcept,
+                                                          RDFOntologyFact skosConceptScheme) {
+            if (ontologyData != null && skosConcept != null && skosConceptScheme != null && !skosConcept.Equals(skosConceptScheme)) {
+
+                //Enforce taxonomy checks
+                if (ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConcept,       RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass())) &&
+                    ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptScheme, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT_SCHEME.ToRDFOntologyClass()))) {
+
+                    // skosConcept -> skos:TopConceptOf -> skosConceptScheme
+                    ontologyData.AddAssertionRelation(skosConcept, (RDFOntologyObjectProperty)RDFSKOSOntology.Instance.Model.PropertyModel.SelectProperty(RDFVocabulary.SKOS.TOP_CONCEPT_OF.ToString()), skosConceptScheme);
+                }
+
+            }
+            return ontologyData;
+        }
+
+        /// <summary>
+        /// Adds the given "skosConceptScheme skos:HasTopConcept skosConcept" assertion to the ontology data
+        /// </summary>
+        public static RDFOntologyData AddSKOSHasTopConcept(this RDFOntologyData ontologyData,
+                                                           RDFOntologyFact skosConceptScheme,
+                                                           RDFOntologyFact skosConcept) {
+            if (ontologyData != null && skosConcept != null && skosConceptScheme != null && !skosConceptScheme.Equals(skosConcept)) {
+
+                //Enforce taxonomy checks
+                if (ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConcept,       RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass())) &&
+                    ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptScheme, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT_SCHEME.ToRDFOntologyClass()))) {
+
+                    // skosConceptScheme -> skos:HasTopConcept -> skosConcept
+                    ontologyData.AddAssertionRelation(skosConceptScheme, (RDFOntologyObjectProperty)RDFSKOSOntology.Instance.Model.PropertyModel.SelectProperty(RDFVocabulary.SKOS.HAS_TOP_CONCEPT.ToString()), skosConcept);
+                }
+
+            }
+            return ontologyData;
+        }
+
+        /// <summary>
+        /// Adds the given "skosConceptA skos:SemanticRelation skosConceptB" assertion to the ontology data
+        /// </summary>
+        public static RDFOntologyData AddSKOSSemanticRelation(this RDFOntologyData ontologyData,
+                                                              RDFOntologyFact skosConceptA,
+                                                              RDFOntologyFact skosConceptB) {
+            if (ontologyData != null && skosConceptA != null && skosConceptB != null && !skosConceptA.Equals(skosConceptB)) {
+
+                //Enforce taxonomy checks
+                if (ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptA, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass())) &&
+                    ontologyData.Relations.ClassType.ContainsEntry(new RDFOntologyTaxonomyEntry(skosConceptB, RDFVocabulary.RDF.TYPE.ToRDFOntologyObjectProperty(), RDFVocabulary.SKOS.CONCEPT.ToRDFOntologyClass()))) {
+
+                    // skosConceptA -> skos:SemanticRelation -> skosConceptB
+                    ontologyData.AddAssertionRelation(skosConceptA, (RDFOntologyObjectProperty)RDFSKOSOntology.Instance.Model.PropertyModel.SelectProperty(RDFVocabulary.SKOS.SEMANTIC_RELATION.ToString()), skosConceptB);
+                }
+
             }
             return ontologyData;
         }
