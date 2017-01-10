fdf702f46ed25dac6786eae1a320726cc8f7b349
Marco De Salvo <mdesalvo@users.noreply.github.com>
2017-01-10 20:01:11 +0100

    (V1.0.0) Begin refactoring of solution

---
+++ RDFSharp.Semantics.Reasoner/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿/*
+   Copyright 2015-2017 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("RDFSharp.Semantics.Reasoner")]
+[assembly: AssemblyDescription("OWL-DL ontology reasoning with RDFSharp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marco De Salvo")]
+[assembly: AssemblyProduct("RDFSharp.Semantics")]
+[assembly: AssemblyCopyright("Marco De Salvo")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3B7436D3-4408-4226-B468-C86988858729")]
