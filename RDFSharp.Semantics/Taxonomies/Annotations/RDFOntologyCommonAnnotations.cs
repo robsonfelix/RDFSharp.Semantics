1c0143e33cc7470b41b1033767bef1eced6f58fb
Marco De Salvo <mdesalvo@users.noreply.github.com>
2017-01-12 20:23:36 +0100

    (V1.0.0) Continue refactoring of solution

---
+++ RDFSharp.Semantics/Taxonomies/Annotations/RDFOntologyCommonAnnotations.cs
@@ -0,0 +1,79 @@
+﻿/*
+   Copyright 2015-2017 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+namespace RDFSharp.Semantics
+{
+
+    /// <summary>
+    /// RDFOntologyCommonAnnotations represents a collector for foundational annotations describing ontology resources.
+    /// </summary>
+    public class RDFOntologyCommonAnnotations {
+
+        #region Properties
+        /// <summary>
+        /// "owl:versionInfo" annotations
+        /// </summary>
+        public RDFOntologyTaxonomy VersionInfo { get; internal set; }
+
+        /// <summary>
+        /// "owl:versionIRI" annotations
+        /// </summary>
+        public RDFOntologyTaxonomy VersionIRI { get; internal set; }
+
+        /// <summary>
+        /// "rdfs:comment" annotations
+        /// </summary>
+        public RDFOntologyTaxonomy Comment { get; internal set; }
+
+        /// <summary>
+        /// "rdfs:label" annotations
+        /// </summary>
+        public RDFOntologyTaxonomy Label { get; internal set; }
+
+        /// <summary>
+        /// "rdfs:seeAlso" annotations
+        /// </summary>
+        public RDFOntologyTaxonomy SeeAlso { get; internal set; }
+
+        /// <summary>
+        /// "rdfs:isDefinedBy" annotations
+        /// </summary>
+        public RDFOntologyTaxonomy IsDefinedBy { get; internal set; }
+
+        /// <summary>
+        /// Custom annotations
+        /// </summary>
+        public RDFOntologyTaxonomy CustomAnnotations { get; internal set; }
+        #endregion
+
+        #region Ctors
+        /// <summary>
+        /// Default-ctor to build an empty ontology common annotations structure
+        /// </summary>
+        internal RDFOntologyCommonAnnotations() {
+            this.VersionInfo       = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Generic);
+            this.VersionIRI        = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Generic);
+            this.Comment           = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Generic);
+            this.Label             = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Generic);
+            this.SeeAlso           = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Generic);
+            this.IsDefinedBy       = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Generic);
+            this.CustomAnnotations = new RDFOntologyTaxonomy(RDFSemanticsEnums.RDFOntologyTaxonomyCategory.Generic);
+        }
+        #endregion
+
+    }
+
+}
