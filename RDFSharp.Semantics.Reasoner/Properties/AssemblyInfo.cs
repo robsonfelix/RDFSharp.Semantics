09ae0821ae2b4cbec0a3f1e1e91f7e920a736206
Marco De Salvo <mdesalvo@users.noreply.github.com>
2018-06-30 14:14:44 +0200

    Move Reasoner to solution project

---
+++ RDFSharp.Semantics.Reasoner/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿/*
+   Copyright 2015-2018 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("RDFSharp.Semantics.Reasoner")]
+[assembly: AssemblyDescription("Reason over OWL-DL ontologies with RDFSharp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Marco De Salvo")]
+[assembly: AssemblyProduct("RDFSharp.Semantics")]
+[assembly: AssemblyCopyright("Marco De Salvo")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("F83FFAD7-0EA5-4F31-AA27-C10F3AEBA13E")]
