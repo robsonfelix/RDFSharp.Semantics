eb2aa33f684c5d96c4f4ce5780e118343bd54d6c
Marco De Salvo <mdesalvo@users.noreply.github.com>
2018-07-01 12:02:25 +0200

    Removed unused usings

--- RDFSharp.Semantics/RDFSemanticsUtilities.cs
+++ RDFSharp.Semantics/RDFSemanticsUtilities.cs
@@ -15,11 +15,9 @@
 */
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using RDFSharp.Model;
-using RDFSharp.Query;
 
 namespace RDFSharp.Semantics
 {
