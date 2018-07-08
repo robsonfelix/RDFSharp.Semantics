8514c0ecf228a4e3c85aadd230eeafeea8c77163
Marco De Salvo <mdesalvo@users.noreply.github.com>
2018-07-08 17:37:27 +0200

    Code reorganizations

---
+++ RDFSharp.Semantics/Reasoner/Library/SKOS/RDFSKOSRuleset.cs
@@ -0,0 +1,50 @@
+﻿/*
+   Copyright 2015-2018 Marco De Salvo
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System;
+using System.Linq;
+using RDFSharp.Model;
+
+namespace RDFSharp.Semantics
+{
+
+    /// <summary>
+    /// RDFSKOSRuleset implements a subset of SKOS entailment rules
+    /// </summary>
+    public static class RDFSKOSRuleset {
+
+        #region Properties
+        /// <summary>
+        /// Counter of rules contained in the SKOS ruleset
+        /// </summary>
+        public static readonly Int32 RulesCount = 0;
+        #endregion
+
+        #region Ctors
+        /// <summary>
+        /// Static-ctor to initialize the SKOS ruleset
+        /// </summary>
+        static RDFSKOSRuleset() {
+
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+
+    }
+
+}
