bbb4d87acf454e9b1bf0640238210379950351d5
Marco De Salvo <mdesalvo@users.noreply.github.com>
2018-07-09 12:08:02 +0200

    initial modeling of SKOSHelper

--- RDFSharp.Semantics/Reasoner/Library/SKOS/RDFSKOSHelper.cs
+++ RDFSharp.Semantics/Reasoner/Library/SKOS/RDFSKOSHelper.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
    Copyright 2015-2018 Marco De Salvo
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +15,7 @@
 */
 
 using System;
+using RDFSharp.Model;
 
 namespace RDFSharp.Semantics.SKOS
 {
@@ -23,6 +24,76 @@ namespace RDFSharp.Semantics.SKOS
     ///  RDFSKOSHelper contains utility methods supporting SKOS modeling and reasoning
     /// </summary>
     public static class RDFSKOSHelper {
+
+        #region AddFacts
+        /// <summary>
+        /// Adds the given fact to the ontology data as instance of "skos:Collection"
+        /// </summary>
+        public static RDFOntologyData AddSKOSCollection(this RDFOntologyData ontologyData, RDFResource collection) {
+            if (ontologyData       != null && collection != null) {
+                var collectionFact  = new RDFOntologyFact(collection);
+                var collectionClass = RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.COLLECTION.ToString());
+
+                //Add fact
+                ontologyData.AddFact(collectionFact);
+
+                //Add classtype relation
+                ontologyData.AddClassTypeRelation(collectionFact, collectionClass);
+            }
+            return ontologyData;
+        }
+
+        /// <summary>
+        /// Adds the given fact to the ontology data as instance of "skos:Concept"
+        /// </summary>
+        public static RDFOntologyData AddSKOSConcept(this RDFOntologyData ontologyData, RDFResource concept) {
+            if (ontologyData    != null && concept != null) {
+                var conceptFact  = new RDFOntologyFact(concept);
+                var conceptClass = RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.CONCEPT.ToString());
+
+                //Add fact
+                ontologyData.AddFact(conceptFact);
+
+                //Add classtype relation
+                ontologyData.AddClassTypeRelation(conceptFact, conceptClass);
+            }
+            return ontologyData;
+        }
+
+        /// <summary>
+        /// Adds the given fact to the ontology data as instance of "skos:ConceptScheme"
+        /// </summary>
+        public static RDFOntologyData AddSKOSConceptScheme(this RDFOntologyData ontologyData, RDFResource conceptScheme) {
+            if (ontologyData          != null && conceptScheme != null) {
+                var conceptSchemeFact  = new RDFOntologyFact(conceptScheme);
+                var conceptSchemeClass = RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.CONCEPT_SCHEME.ToString());
+
+                //Add fact
+                ontologyData.AddFact(conceptSchemeFact);
+
+                //Add classtype relation
+                ontologyData.AddClassTypeRelation(conceptSchemeFact, conceptSchemeClass);
+            }
+            return ontologyData;
+        }
+
+        /// <summary>
+        /// Adds the given fact to the ontology data as instance of "skos:OrderedCollection"
+        /// </summary>
+        public static RDFOntologyData AddSKOSOrderedCollection(this RDFOntologyData ontologyData, RDFResource orderedCollection) {
+            if (ontologyData              != null && orderedCollection != null) {
+                var orderedCollectionFact  = new RDFOntologyFact(orderedCollection);
+                var orderedCollectionClass = RDFSKOSOntology.Instance.Model.ClassModel.SelectClass(RDFVocabulary.SKOS.ORDERED_COLLECTION.ToString());
+
+                //Add fact
+                ontologyData.AddFact(orderedCollectionFact);
+
+                //Add classtype relation
+                ontologyData.AddClassTypeRelation(orderedCollectionFact, orderedCollectionClass);
+            }
+            return ontologyData;
+        }
+        #endregion
 
     }
 
